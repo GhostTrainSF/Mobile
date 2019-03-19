e38edaf1154e54fd1e6900b8e1574d223574b742
ecuyle <ecuyle@gmail.com>
2019-03-19 01:15:29 -0700

    Refactor App to use sub-components

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {
-  Text,
   View,
-  StyleSheet,
-  Picker,
-  ScrollView,
 } from 'react-native';
+
+import CurrentEstimates from './Components/CurrentEstimates.js';
+import Stops from './Components/Stops.js';
+import Routes from './Components/Routes.js';
+import Header from './Components/Header.js';
 
 export default class App extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ export default class App extends Component {
     this.state = {
       availableStops: [],
       currentEstimates: [],
+      currentDelays: [0, 0, 0],
       selectedRoute: 'L - Inbound',
       selectedStop: '',
     }
@@ -47,12 +49,14 @@ export default class App extends Component {
     const { selectedRoute, selectedStop } = this.state;
     const route = selectedRoute.split('-')[0].trim();
     const url = `http://ec2-3-18-105-71.us-east-2.compute.amazonaws.com/v1/api/predictions/${route}/${selectedStop}`;
+    const delaysUrl = `http://ec2-3-18-105-71.us-east-2.compute.amazonaws.com/v1/api/initDelay/${route}/${selectedStop}`;
     try {
-      const results = await fetch(url);
-      let currentEstimates = await results.json();
+      const currEstResults = await fetch(url);
+      let currentEstimates = await currEstResults.json();
+      const currentDelaysResults = await fetch(delaysUrl);
+      let currentDelays = await currentDelaysResults.json();
       if (!Array.isArray(currentEstimates)) currentEstimates = [];
-      console.log(url);
-      this.setState({ currentEstimates });
+      this.setState({ currentEstimates, currentDelays });
     } catch(e) {
       console.log(e);
     }
@@ -72,69 +76,21 @@ export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "stretch" }}>
-        <View style={{ height: 100, backgroundColor: "#333", justifyContent: 'flex-end', alignItems: 'center' }}>
-          <Text style={{ color: '#fff', fontSize: 30 }}>GhostTrain</Text>
-          <Text style={{ color: '#fff' }}>San Francisco</Text>
-        </View>
-        <View style={{ flex: 1, paddingTop: 25, paddingRight: 25, paddingLeft: 25, backgroundColor: '#f7f7f7' }}>
-          <Text>Select a route:</Text>
-          <Picker
-            style={{ borderRadius: 15, backgroundColor: '#fff' }}
-            selectedValue={this.state.selectedRoute}
-            onValueChange={(value) => this.handleRoutePick(value)}
-          >
-            <Picker.Item label="L - Inbound" value="L - Inbound" />
-            <Picker.Item label="L - Outbound" value="L - Outbound" />
-            <Picker.Item label="KT - Inbound" value="KT - Inbound" />
-            <Picker.Item label="KT - Outbound" value="KT - Outbound" />
-            <Picker.Item label="M - Inbound" value="M - Inbound" />
-            <Picker.Item label="M - Outbound" value="M - Outbound" />
-            <Picker.Item label="N - Inbound" value="N - Inbound" />
-            <Picker.Item label="N - Outbound" value="N - Outbound" />
-            <Picker.Item label="J - Inbound" value="J - Inbound" />
-            <Picker.Item label="J - Outbound" value="J - Outbound" />
-          </Picker>
-        </View>
-        <View style={{ flex: 1, paddingTop: 25, paddingRight: 25, paddingLeft: 25, backgroundColor: '#f7f7f7' }}>
-          <Text>Select a stop:</Text>
-          <Picker
-            style={{ borderRadius: 15, backgroundColor: '#fff' }}
-            selectedValue={this.state.selectedStop}
-            onValueChange={(value) => this.handleStopPick(value)}
-          >
-            {
-              this.state.availableStops.map((stop, i) => {
-                return <Picker.Item label={stop.title} value={stop.tag} key={i} />
-              })
-            }
-          </Picker>
-        </View>
-        <View style={{ flex: 1, paddingTop: 25, paddingRight: 25, paddingLeft: 25, paddingBottom: 50, backgroundColor: '#f7f7f7' }}>
-          <Text>Current Estimates:</Text>
-          <ScrollView>
-            {
-              this.state.currentEstimates.length > 0
-                ? this.state.currentEstimates.map((est, i) => {
-                    if (parseInt(est) === 0) return;
-                    return (<Text style={styles.item} key={i}>{est} min</Text>);
-                  })
-                : <Text style={styles.item}>Error loading estimates</Text>
-            }
-          </ScrollView>
-        </View>
+        <Header />
+        <Routes
+          selectedRoute={this.selectedRoute}
+          handleRoutePick={this.handleRoutePick}
+        />
+        <Stops
+          handleStopPick={this.handleStopPick}
+          selectedStop={this.state.selectedStop}
+          availableStops={this.state.availableStops}
+        />
+        <CurrentEstimates
+          currentEstimates={this.state.currentEstimates}
+          currentDelays={this.state.currentDelays}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 44,
-    color: '#909090',
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: '#f7f7f7' 
-  },
-});
